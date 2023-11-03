1ec58df9d29b6e58131c5d78e6eda6d4c8a446fe
Francisco Franco <franco_siete@yahoo.com>
2023-11-03 12:38:14 -0700

    Added module

---
+++ src/main/python/algorithms/two-pointers/squares_of_sorted_array.py
@@ -0,0 +1,41 @@
+"""
+Given an integer array nums sorted in non-decreasing order, 
+return an array of the squares of each number sorted in 
+non-decreasing order.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+"""
+
+# Function which sorts array after doing squares of elements
+def sortSquares(arr, n):
+    left, right = 0, n - 1
+    index = n - 1
+    result = [0 for x in arr]
+ 
+    while index >= 0:
+ 
+        if abs(arr[left]) >= abs(arr[right]):
+            result[index] = arr[left] * arr[left]
+            left += 1
+        else:
+            result[index] = arr[right] * arr[right]
+            right -= 1
+        index -= 1
+     
+    for i in range(n): 
+        arr[i] = result[i]
+ 
+# Driver code 
+arr = [-6, -3, -1, 2, 4, 5 ] 
+n = len(arr) 
+ 
+print("Before sort ") 
+for i in range(n): 
+    print(arr[i], end =" " ) 
+     
+sortSquares(arr, n) 
+print("\nAfter Sort ") 
+for i in range(n): 
+    print(arr[i], end =" " ) 
