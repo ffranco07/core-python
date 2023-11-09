4bc6dac516ba43b5239227b3034c8dc98737a3a8
Francisco Franco <franco_siete@yahoo.com>
2023-11-09 14:42:38 -0800

    Refactored logic

--- src/main/python/algorithms/fiboseq.py
+++ src/main/python/algorithms/fiboseq.py
@@ -1,32 +1,70 @@
-# Program to display the Fibonacci sequence up to nth term where n is provided by the user
+"""
+ Author: Francisco Franco
+ 
+ Creates a series of numbers in which each number 
+ (Fibonacci Number) is the sum of the two preceding numbers.
 
-# change this value for a different result
-nterms = 8
+ f(n) = f(n-1) + f(n-2) for n > 1 and f(0) = 0 and f(1) = 1
 
-# uncomment to take input from the user
-#nterms = int(input("How many terms? "))
+"""
 
-# first two terms
-n1 = 0
-n2 = 1
-count = 0
+# Recursive function to calculate fibonacci sequence sum at n
+# NOT AS EFFICIENT as iterative function below
+#
+# Top-down approach 
+# Start from the top (the original problem) and move down toward the base cases
+#
+# Time Complexity: O(2^n) Upper Bound with Exponential Time
+# Space Complexity: O(1)
+def fibo_recursive(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibo_recursive(n - 1) + fibo_recursive(n - 2)
 
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto:",nterms)
-   print(n1)
-else:
-   print("Fibonacci sequence upto:",nterms)
-   while count < nterms:
-       if count < nterms - 1:
-         # ends the output with a commma and space instead of default newline
-         print(n1,end=", ")
-       else:
-          print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+# Recursive function to calculate fibonacci sequence sum at n
+# using dictionary/map memoization to avoid repeated recursive computations.
+# When we find the answer (the return value) for a given 
+# recursive step state, we cache that value in dictionary/map for resuse. 
+#
+# Top-down approach 
+# Start from the top (the original problem) and move down toward the base cases
+#
+# Time Complexity: O(n) Upper Bound with Exponential Time
+# Space Complexity: O(1)
+def fibo_recursive_dp(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return memo[n]
+
+
+# Iterative function to calculate fibonacci sequence sum at n
+#
+# Bottom-up approach 
+# Start at the bottom (base cases) and work our way up to larger problems
+#
+# Time Complexity: O(n) Upper Bound with Exponential Time
+# Space Complexity: O(1)
+def fibo_iterative(n):
+    arr = [0] * (n + 1)
+    # base case - the second Fibonacci number is 1
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    
+    return arr[n]
+
+memo = {}
+size = 6
+print("size: " + str(size) + " fiboRecursive: " + str(fibo_recursive(size)))
+print("size: " + str(size) + " fiboRecursiveDP: " + str(fibo_recursive(size)))
+print("size: " + str(size) + " fiboIterative: " + str(fibo_recursive(size)))
+
